09dbe1a5b1840d34fe9128cd5c56ae75b169e657
fhub-1 <71527527+fhub-1@users.noreply.github.com>
2021-10-29 11:15:37 +0300

    Adding same useful link and account ttch YouTube

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,25 +1,54 @@
 import { useRouter } from "next/router";
 import NextLink from "next/link";
-import { Stack, VStack, Divider, Link, SimpleGrid } from "@chakra-ui/react";
+import { VStack, Divider, Link, SimpleGrid } from "@chakra-ui/react";
+import {
+  TWITTER_PROFILE,
+  GITHUB_PROFILE,
+  YOUTUBE_CHANNEL,
+  POLYWORK_PROFILE,
+  TWITCH_CHANNEL
+} from "../../constants";
 
-const firstGroup= [
+const firstGroup = [
   {
-  href: '/',
-  label: 'Home'
-},
-{
-  href: '/blog',
-  label: 'Blog',
-},
-{
-  href: '/colophon',
-  label: 'Colophon',
-},
-{
-  href: '/talks',
-  label: 'Talks',
-},
-]
+    href: "/",
+    label: "Home"
+  },
+  {
+    href: "/blog",
+    label: "Blog"
+  },
+  {
+    href: "/colophon",
+    label: "Colophon"
+  },
+  {
+    href: "/talks",
+    label: "Talks"
+  }
+];
+const secondGroup = [
+  {
+    href: "/",
+    label: "Twitter"
+  },
+  {
+    href: "/",
+    label: "GitHub"
+  },
+  {
+    href: "/",
+    label: "Youtube"
+  },
+  {
+    href: "/talks",
+    label: "Polywork"
+  },
+  {
+    href: "/talks",
+    label: "Twitch"
+  }
+];
 
 const Footer = () => {
   const { pathname } = useRouter();
@@ -28,21 +57,14 @@ const Footer = () => {
     <VStack pb={8} spacing={8}>
       <Divider />
       <SimpleGrid columns={{ base: 1, md: 3 }} w="full">
-        <VStack alignItems="flex-start">
-          <NextLink href="/home" passHref>
-            <Link color={pathname === "/" ? "purple.500" : "gray.500"}>
-              Home
-            </Link>
-          </NextLink>
-          <NextLink href="/blog" passHref>
-            <Link>Blog</Link>
-          </NextLink>
-          <NextLink href="/colophon" passHref>
-            <Link>Colophon</Link>
-          </NextLink>
-          <NextLink href="/talks" passHref>
-            <Link>Talks</Link>
-          </NextLink>
+        <VStack alignItems="flex-start" textDecoration="none">
+          {firstGroup.map(({ href, label }) => (
+            <NextLink key={href} href={href} passHref>
+              <Link color={pathname === href ? "purple.500" : "gray.700"}>
+                {label}
+              </Link>
+            </NextLink>
+          ))}
         </VStack>
       </SimpleGrid>
     </VStack>
---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const TWITTER_PROFILE = "https://twitter.com/@josehub121";
+export const GITHUB_PROFILE = "https://github.com/fhub-1";
+export const YOUTUBE_CHANNEL =
+  "https://www.youtube.com/channel/UCAOdLWYUTCgX5PjNSI2DNIg";
+export const FACEBOOK_GROUP =
+  "https://www.facebook.com/groups/3343365749039855/";
+export const TWITCH_CHANNEL = "https://twitch.tv/HackTheCoding";
