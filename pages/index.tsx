e937a9316e0516214a39906f4e928a9406da8f2a
fhub-1 <71527527+fhub-1@users.noreply.github.com>
2021-11-01 16:40:26 +0300

    adding same cool staff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,34 @@
+import {
+  VStack,
+  HStack,
+  Stack,
+  Heading,
+  IconButton,
+  Icon,
+  Text
+} from "@chakra-ui/react";
+import { HiPlay, HiPause } from "react-icons/hi";
 
-const IndexPage = () => <h1>Hello Next.js and Chakra UI 👋</h1>;
-export default IndexPage
+const IndexPage = () => (
+  <VStack spacing={20}>
+    <Stack>
+      <VStack spacing={3} alignItems="flex-start">
+        <HStack spacing={3}>
+          <Heading size="lg">Hi, I'm Joseph Kitheka.</Heading>
+          <IconButton
+            aria-label="Play pronunciation track"
+            icon={<Icon color="purple.400" as={HiPlay} w={6} h={6} />}
+            variant="unstyled"
+            size="xs"
+            rounded="full"
+          />
+        </HStack>
+        <Text>
+          I'm a Self-Taught-Developer that loves building products and web
+          applications that impact millions of lives. Currently working on
+        </Text>
+      </VStack>
+    </Stack>
+  </VStack>
+);
+export default IndexPage;
